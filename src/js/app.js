7375524a1361e77aae97db58621d989cae94a5fd
Nadia Di Ciocco <nadiadiciocco54@gmail.com>
2023-10-09 16:45:47 +0200

    devuelta al principio

--- src/js/app.js
+++ src/js/app.js
@@ -108,7 +108,7 @@ function paginaSiguiente() {
  async function consultarAPI() {
 
     try {
-        const url = `${location.origin}/api/servicios`;
+        const url = '/api/servicios';
         const resultado = await fetch(url);
         const servicios = await resultado.json();
         mostrarServicios(servicios);
@@ -323,7 +323,7 @@ async function reservarCita() {
 
     try {
         // Peticion hacia la API
-        const url = `${location.origin}/api/citas`;
+        const url = '/api/citas';
 
         const respuesta = await fetch(url, {
             method: 'POST',
